21ed77511cfce016e63369fdcf2b86767ace945d
Aakriti Shroff <aakriti@mit.edu>
2015-01-04 11:28:54 -0500

    fixing merges, pulling changes

---
+++ op/acl.go
@@ -0,0 +1,138 @@
+/*
+ACL module that manages permissions on datapages.
+Currently represented as an ACL struct and stored in
+the datapage's
+page.appData["isPublic"] --> true/false and
+page.appData["readers"] -->[]string (== userIDs of authorized readers)
+
+*/
+
+package op
+
+import (
+	db "../data/inmem"
+	"log"
+)
+
+var propPub = "isPublic"    //constant variable used to access acl property in datapage's appData
+var propReaders = "readers" //constant variable used to access acl property in datapage's appData
+
+/*
+If owner doesn't set isPublic property during creation, we set it to false, and add owner to readers.
+If owner sets isPublic to false, we add owner to readers (append to list if readers already set by owner)
+If owner sets to true
+*/
+func createACL(page *db.Page, owner string, isPub bool) {
+	page.Set(propPub, isPub)
+
+	oldReaders, ok := page.Get(propReaders)
+	if !ok {
+		page.Set(propReaders, []string{owner})
+	} else {
+		newReaders := oldReaders.([]string)
+		newReaders = append(newReaders, owner)
+		page.Set(propReaders, newReaders)
+		log.Printf("Created ACL... Owner: %s, isPublic: %t, Readers: %s", owner, isPub, newReaders)
+	}
+
+}
+
+/*
+Checks if user with userID can read datapage
+*/
+func isReadable(userID string, page *db.Page) bool {
+	isPublic, readers, ok := getACL(page)
+	if !ok {
+		return false
+	}
+	if isPublic {
+		return true
+	}
+	for _, v := range readers {
+		if v == userID {
+			return true
+		}
+
+	}
+	return false //not found in readers and isPublic == false
+
+}
+
+/*
+Checks if user with userID can write datapage. Must be owner
+*/
+func isWritable(userID string, page *db.Page) bool {
+	val, ok := page.Get("_owner")
+	if ok {
+		owner := val.(string)
+		return owner == userID
+	}
+	return false
+}
+
+/*
+Get ACL for datapage
+Returns (false, []string{"uid1", "uid2"}) or (false, []string{}) (empty slice if isPublic == true)
+*/
+func getACL(page *db.Page) (isPublic bool, readers []string, ok bool) {
+	val1, exists1 := page.Get(propPub)
+	if exists1 {
+		isPublic = val1.(bool)
+		if !isPublic {
+			val2, exists2 := page.Get(propReaders)
+			if exists2 {
+				readers = val2.([]string)
+				ok = true
+			} else {
+				log.Println("isPublic is false, but readers property not found! Shouldn't reach here!")
+				ok = false
+			}
+		} else {
+			readers = []string{} //isPublic = true, readers = empty slice
+			ok = true
+		}
+	} else {
+		log.Println("isPublic property not found! Either page doesn't exist or shouldn't reach here!")
+		ok = false
+	}
+	return
+
+}
+
+/*
+Reset ACL, remove all readers except owner
+*/
+func resetACL(page *db.Page, owner string) {
+	page.Set(propPub, false)
+	page.Set(propReaders, []string{owner})
+}
+
+/*
+Adds userID to readers and make sure isPublic == false
+*/
+func addReader(userID string, page *db.Page) {
+	page.Set(propPub, false)
+	oldReaders, ok := page.Get(propReaders)
+	if !ok {
+		page.Set(propReaders, []string{userID})
+	} else {
+		newReaders := oldReaders.([]string)
+		newReaders = append(newReaders, userID)
+		page.Set(propReaders, newReaders)
+	}
+}
+
+/*
+Revokes userID
+*/
+func revokeReader(userID string, page *db.Page) {
+	_, readers, ok := getACL(page)
+	if ok {
+		newReaders := []string{}
+		for _, v := range readers {
+			if v != userID {
+				newReaders = append(newReaders, v)
+			}
+		}
+	}
+}
---
+++ op/acl_test.go
@@ -0,0 +1,158 @@
+package op
+
+// import (
+// 	"code.google.com/p/go.net/websocket"
+// 	"encoding/json"
+// 	//"fmt"
+// 	//db "github.com/aakritishroff/datapages/inmem"
+// 	"log"
+// 	"testing"
+// )
+
+// var origin = "http://localhost/"
+// var url = "ws://localhost:8080/.well-known/podsocket/v1"
+
+// func setupUser(username string, data []string) *websocket.Conn {
+// 	dataMap := make(map[string]interface{})
+// 	dataMap["userId"] = username //"aakriti/"
+// 	opLoginData, _ := json.Marshal(InMessage{Seq: 1, Op: "login", Data: dataMap})
+
+// 	ws, err := websocket.Dial(url, "", origin)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+
+// 	_, err = ws.Write(opLoginData)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+
+// 	log.Printf("Logging in as %s\n", username)
+
+// 	//Create a few Pages
+// 	dataMap["initialData"] = data[0] //"Public R+W Hello World!"
+// 	opCreatePageData, _ := json.Marshal(InMessage{Seq: 1, Op: "create", Data: dataMap})
+// 	_, err = ws.Write(opCreatePageData)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	log.Printf("Creating pages.. First Page Sent: %s\n", opCreatePageData)
+
+// 	dataMap["initialData"] = data[1] //"Public R+W Hello World Once Again!"
+// 	opCreatePageData, _ = json.Marshal(InMessage{Seq: 1, Op: "create", Data: dataMap})
+// 	_, err = ws.Write(opCreatePageData)
+
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+
+// 	log.Printf("Creating pages.. Second Page Sent: %s\n", opCreatePageData)
+
+// 	return ws
+
+// }
+
+// func Test_isReadableACL(t *testing.T) {
+
+// 	wsAlice := setupUser("alice/", []string{"Alice's Public Page 0 ", "Alice's Public Page 1"})
+
+// 	dataMap := make(map[string]interface{})
+// 	dataMap["_id"] = "alice/a0"
+// 	opReadPage, _ := json.Marshal(InMessage{Seq: 1, Op: "read", Data: dataMap})
+
+// 	_, err := wsAlice.Write(opReadPage)
+
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+
+// 	log.Printf("Sending Read Req... %s\n", opReadPage)
+// }
+
+// func Test_isWritableACL(t *testing.T) {
+// 	wsAlice := setupUser("alice/", []string{"Alice's Public Page 2 ", "Alice's Public Page 3"})
+// 	wsBob := setupUser("bob/", []string{"Bob's Public Page 2", "Bob's Public Page 3"})
+
+// 	dataMap := make(map[string]interface{})
+// 	dataMap["_id"] = "alice/a0"
+// 	dataMap["_etag"] = 0
+// 	dataMap["randomProp"] = "Booyah"
+
+// 	opUpdatePage, _ := json.Marshal(InMessage{Seq: 1, Op: "update", Data: dataMap})
+
+// 	_, err1 := wsBob.Write(opUpdatePage)
+
+// 	if err1 != nil {
+// 		log.Fatal(err1)
+// 	}
+
+// 	dataMap = make(map[string]interface{})
+// 	dataMap["_id"] = "alice/a0"
+// 	dataMap["_etag"] = 0
+// 	dataMap["randomProp"] = "Yeah"
+
+// 	opUpdatePage, _ = json.Marshal(InMessage{Seq: 1, Op: "update", Data: dataMap})
+
+// 	_, err2 := wsAlice.Write(opUpdatePage)
+
+// 	if err2 != nil {
+// 		log.Fatal(err2)
+// 	}
+// 	log.Printf("Sending Update Req... %s\n", opUpdatePage)
+// }
+
+// func Test_makePublic(t *testing.T) {
+
+// }
+
+// /*
+// setupUser("bob/", []string{"Bob's Public Page", "Bob's Private Page"})
+
+// isReadable("alice/", alicePage)
+// isWritable("alice/", alicePage)
+
+// if isReadable("bob/", alicePage) {
+// 	t.Fail()
+// }
+
+// bobPage, _ := cluster.PageByURL("bob/a0", false)
+// isReadable("bob/", bobPage)
+// isWritable("bob/", bobPage)
+
+// if isReadable("alice/", bobPage) {
+// 	t.Fail()
+// }*/
+
+// /*	c := db.NewInMemoryCluster("http://cluster.as1.crosscloud.org")
+// 	p1, p1x := c.NewPod("http://pod1.as1.crosscloud.org")
+// 	if p1x {
+// 		t.Fail()
+// 	}
+// 	g1, _ := p1.NewPage()
+// 	if g1.URL() != "http://pod1.as1.crosscloud.org/auto/0" {
+// 		t.Fail()
+// 	}*/
+
+// /*dataMap = make(map[string]interface{})
+// dataMap["initialData"] = "Public R+W Hello World Third time's the charm!"
+// opCreatePage = InMessage{Seq: 1, Op: "create", Data: dataMap}
+// opCreatePageData, _ = json.Marshal(opCreatePage)
+// _, err = ws.Write(opCreatePageData)
+// if err != nil {
+// 	log.Fatal(err)
+// }
+
+// log.Printf("Creating pages.. Third Page Sent: %s\n", opCreatePageData)
+
+// var opCreatePageResp = make([]byte, 512)
+// _, err = ws.Read(opCreatePageResp)
+// if err != nil {
+// 	log.Fatal(err)
+// }
+// log.Printf("Creating pages.. Third Page Received: %s\n", opCreatePageResp)
+// */
+
+// /*setupUser("alice/", []string{"Alice's Public Page", "Alice's Private Page", "Alice's Shared Page (Alice + Bob)"})
+// setupUser("bob/", []string{"Bob's Public Page", "Bob's Private Page", "Bob's Shared Page (Alice + Bob)"})
+// setupUser("eve/", []string{"Eve's Public Page", "Eve's Private Page", "Eve's Shared Readable Page (Alice + Eve)"})
+// */
